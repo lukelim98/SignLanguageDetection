98b6327dd4548d38092aa4c008ade0d8b3a79644
Joo Wan Lim <joowan980330@gmail.com>
2025-01-21 18:29:07 -0800

    Update collect_imgs.py and add data directory

--- collect_imgs.py
+++ collect_imgs.py
@@ -9,6 +9,30 @@ if not os.path.exists(DATA_DIR):
 number_of_classes = 3
 dataset_size = 100
 
-cv2.VideoCapture(0, cv2.CAP_V4L2)
+cap = cv2.VideoCapture(0)
 
+for j in range(number_of_classes):
+    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
+        os.makedirs(os.path.join(DATA_DIR, str(j)))
 
+    print(f'Collectin data for class {j}')
+    done = False
+    while True:
+        ret, frame = cap.read()
+        cv2.putText(frame, 'Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) == ord('q'):
+            break
+
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+
+        counter += 1
+
+cap.release()
+cv2.destroyAllWindows()
